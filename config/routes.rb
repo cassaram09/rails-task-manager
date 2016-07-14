17bdf252cbcfca92fe99d61d6a6afd0c89a02906
cassaram09 <cassaram09@gmail.com>
2016-07-14 10:21:21 -0400

    created home controller, generated devise user model, added role migration, edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
+  devise_for :users, skip: [:sessions] 
+    as :user do
+      get 'login' => 'devise/sessions#new', :as => :new_user_session
+      post 'login' => 'devise/sessions#create', :as => :user_session
+      delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+    end
+
+  devise_scope :user do
+    get "/signup" => "devise/registrations#new"
+    get "/edit-profile" => "devise/registrations#edit"
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
