1a5a0aa1403a472e0d6df4010bac25f6903dd457
cassaram09 <cassaram09@gmail.com>
2016-07-14 11:42:09 -0400

    added project update and delete functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :set_user, except: [:destroy]
+  before_action :set_project, except: [:index, :new, :create]
 
   def index
     @projects = @user.projects
@@ -22,14 +23,22 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    @project.update(project_params)
+    redirect_to project_path(@project)
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
   def set_user
     @user = current_user
+  end
+
+  def set_project
+    @project = Project.find_by(id: params[:id])
   end
 
   def project_params
